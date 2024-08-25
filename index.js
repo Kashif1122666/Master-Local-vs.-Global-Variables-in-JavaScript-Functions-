beba9ef3088aaa820c72a3f37e4fb7fc15aefe40
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-08-25 12:51:35 -0700

    Add files via upload
    Done with Master Local vs. Global Variables in JavaScript Functions!

---
+++ index.js
@@ -0,0 +1,29 @@
+// chapter - 38: functions: Local vs Global variables
+
+// global variable accessable inside and outside of function
+
+// const name1 = "ahmadAli";  //  accessable inside and outside of function
+
+// function printname1() {
+//    console.log(name1);
+// }
+
+// print directly 
+// console.log(name1);
+
+// print using function
+// printname1();
+
+// --------------------------------
+
+// local variable accessable only inside function
+
+// function printname1() {
+    // const name1 = "ahmad"; //local variable accessiable only inside function
+    // console.log(name1);
+//  }
+
+ // print using function
+// printname1();
+
+// ----------------------------------
